5ce062b929ffb9b66bf2245095d9ac1b860c344a
Arctaea <joycacao@gmail.com>
2024-10-29 16:27:18 +0800

    today add calorie intake

--- controllers/todaysController.js
+++ controllers/todaysController.js
@@ -1,11 +1,11 @@
 import { Today } from '../models/todaysModel.js';
-import { publicIntakeValidation } from '../validation/validation.js'; 
+import { intakeValidation, publicIntakeValidation } from '../validation/validation.js'; 
 
 const getAllTodays = async (_req, res, next) => {
     try {
       const result = await Today.find();
       res.status(200).json(result);
-    } catch (error) {next(error);}
+    } catch (error) {res.status(500).json({ message: error.message });}
   }
 
 const getTodayById = async (req, res, next) => {
@@ -15,18 +15,40 @@ const getTodayById = async (req, res, next) => {
   
       if(!result) {return res.status(404).json({ message: 'Date Not Found' });}
       res.status(200).json(result);
-    } catch (error) {next(error);}
+    } catch (error) {res.status(500).json({ message: error.message });}
   }
 
-const getIntake = async (req, res, next) => {
+const dailyIntake = async (req, res, next) => {
   try {
-    const { error } = publicIntakeValidation.validate(req.body);
+    const { date, height, age, weight, weightDesired, blood, dailyRate } = req.body;
+
+    const existingDate = await Today.findOne({ date });
+    if(existingDate) {return res.status(409).json({ message: 'Date already registered' });}
+
+    const { error } = intakeValidation.validate(req.body);
     if(error) {return res.status(400).json({ message: 'Missing required fields' });}
 
-    const { height, age, weight, weightDesired, blood } = req.body;
-    res.status(200).json({
-      height, age, weight, weightDesired, blood
+    const newIntake = await Today.create({
+      date,
+      height,
+      age,
+      weight,
+      weightDesired,
+      blood,
+      dailyRate,
     });
-  } catch (error) {next(error);}
+    res.status(200).json({
+      today: {
+        date: newIntake.date.toISOString().split('T')[0],
+        height: newIntake.height,
+        age: newIntake.age,
+        weight: newIntake.weight,
+        weightDesired: newIntake.weightDesired,
+        blood: newIntake.blood,
+        dailyRate: newIntake.dailyRate,
+      }
+    });
+  } catch (error) {res.status(500).json({ message: error.message });}
 }
-export { getAllTodays, getTodayById, getIntake };
+
+export { getAllTodays, getTodayById, dailyIntake };
