e29c2bd5e4a72cc6c289d315cfed8aa006266fa7
Arctaea <joycacao@gmail.com>
2024-10-29 16:30:37 +0800

    route for add intake, update product consumed, and delete product consumed

--- routes/api/todaysRouter.js
+++ routes/api/todaysRouter.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { getAllTodays, getTodayById, getPublicIntake } from '../../controllers/todaysController.js';
+import { getAllTodays, getTodayById, dailyIntake } from '../../controllers/todaysController.js';
 import { authenticateToken } from '../../middlewares/auth.js';
 
 const router = express.Router();
 
 router.get('/', authenticateToken, getAllTodays);
 router.get('/:todayId', authenticateToken, getTodayById);
-router.get('/public', getPublicIntake);
-
+router.post('/', authenticateToken, dailyIntake);
+router.patch('/:todayId/:productId', authenticateToken, consumeProduct);
+router.delete('/:todayId/:productId', authenticateToken, deleteConsumeProduct);
 // router.post('/today', authenticateToken, addToday);
 // router.delete('/:todayId', authenticateToken, deleteToday);
 // router.put('/:todayId', authenticateToken, updateToday);
