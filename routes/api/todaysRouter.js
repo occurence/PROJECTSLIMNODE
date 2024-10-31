3efd9b7706e61017515a24827ffe43fbfdc77401
Arctaea <joycacao@gmail.com>
2024-10-31 19:01:02 +0800

    route for delete today by whole date

--- routes/api/todaysRouter.js
+++ routes/api/todaysRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getAllTodays, getTodayById, dailyIntake, consumeProduct, deleteConsumeProduct } from '../../controllers/todaysController.js';
+import { getAllTodays, getTodayById, deleteTodayById, dailyIntake, consumeProduct, deleteConsumeProduct } from '../../controllers/todaysController.js';
 import { authenticateToken } from '../../middlewares/auth.js';
 
 const router = express.Router();
 
 router.get('/', authenticateToken, getAllTodays);
 router.get('/:todayId', authenticateToken, getTodayById);
+router.delete('/:todayId', authenticateToken, deleteTodayById);
 router.post('/', authenticateToken, dailyIntake);
 router.patch('/:todayId', authenticateToken, consumeProduct);
 router.delete('/:todayId/:productId', authenticateToken, deleteConsumeProduct);
