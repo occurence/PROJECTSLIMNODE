ce9e53d9a82787c75f33a4faa39c6a53d1aad490
Arctaea <joycacao@gmail.com>
2024-11-01 20:18:57 +0800

    convert date format

--- controllers/todaysController.js
+++ controllers/todaysController.js
@@ -9,7 +9,8 @@ const getAllTodays = async (req, res, next) => {
       const todayData = results.map(result => ({
         _id: result._id,
         user: result.user,
-        date: result.date.toISOString().split('T')[0], // Format the date
+        // date: result.date.toISOString().split('T')[0], // Format the date
+        date: result.date,
         height: result.height,
         age: result.age,
         weight: result.weight,
@@ -19,26 +20,10 @@ const getAllTodays = async (req, res, next) => {
         consumed: result.consumed,
         left: result.left,
         percentage: result.percentage,
-        product: result.product,
+        products: result.products,
     }));
 
     res.status(200).json(todayData);
-      // res.status(200).json({
-      //   today: {
-      //     user: result.user,
-      //     date: result.date.toISOString().split('T')[0],
-      //     height: result.height,
-      //     age: result.age,
-      //     weight: result.weight,
-      //     weightDesired: result.weightDesired,
-      //     blood: result.blood,
-      //     dailyRate: result.dailyRate,
-      //     consumed: result.consumed,
-      //     left: result.left,
-      //     percentage: result.percentage,
-      //     product: result.product,
-      //   },
-      // });
     } catch (error) {res.status(500).json({ message: error.message });}
 }
 
@@ -64,21 +49,33 @@ const dailyIntake = async (req, res, next) => {
   try {
     const { _id } = req.user;
     const { user, date, height, age, weight, weightDesired, blood, dailyRate } = req.body;
-
-    // const existingDate = await Today.findOne({ date });
-    // const existingDateForUser = await Today.find({ user: { $regex: new RegExp(_id, 'i') } });
+    console.log(req.body);
+    let dateObject;
+    let dateString;
+    const formatDate = new Date(date);
+    if (date.includes('T')) {
+      dateObject = new Date(date); // Directly convert the ISO date
+    } else {
+      dateString = `${date}T00:00:00Z`; // Append time for midnight in UTC
+      dateObject = new Date(dateString); // Create a Date object
+    }
+    console.log('Date:', date);
+    console.log('DateString:', dateString)
+    console.log('DateObject:', dateObject);
     const existingDateForUser = await Today.findOne({
       user: { $regex: new RegExp(_id, 'i') },
-      date: date,
+      date,
     });
-    
+
     if(existingDateForUser) {return res.status(409).json({ message: 'Date already registered' });}
 
     const { error } = intakeValidation.validate(req.body);
     if(error) {return res.status(400).json({ message: 'Missing required fields' });}
 
-    const newIntake = await Today.create({
+    const result = await Today.create({
       user,
+      // date: dateObject,
+      // date: formatDate,
       date,
       height,
       age,
@@ -87,18 +84,7 @@ const dailyIntake = async (req, res, next) => {
       blood,
       dailyRate,
     });
-    res.status(200).json({
-      today: {
-        user: newIntake.user,
-        date: newIntake.date.toISOString().split('T')[0],
-        height: newIntake.height,
-        age: newIntake.age,
-        weight: newIntake.weight,
-        weightDesired: newIntake.weightDesired,
-        blood: newIntake.blood,
-        dailyRate: newIntake.dailyRate,
-      }
-    });
+    res.status(200).json(result);
   } catch (error) {res.status(500).json({ message: error.message });}
 }
 
@@ -106,22 +92,31 @@ const consumeProduct = async (req, res, next) => {
   try {
     const { todayId } = req.params;
     const result = await Today.findById(todayId);
+    // const result = await Today.findByIdAndUpdate(todayId, req.body, { new: true, runValidators: true });
     if(!result) {return res.status(404).json({ message: 'Date not Found' });}
 
-    if (!result.product) {result.product = new Map();}
+    // if (!result.products) {result.products = new Map();}
     
-    for (const [productId, grams] of Object.entries(req.body.product)) {
-      result.product.set(productId, grams);
-    }
+    // for (const [productId, grams] of Object.entries(req.body.product)) {
+    //   result.products.set(productId, grams);
+    // }
 
+    // await result.save();
+    const addProduct = {
+      productId: req.body.products[0].productId,
+      productName: req.body.products[0].productName,
+      grams: req.body.products[0].grams,
+    };
+    result.products.push(addProduct);
     await result.save();
 
-    res.status(200).json({
-      today: {
-        product:
-          result.product,
-      },
-    });
+    res.status(200).json(result);
+    // res.status(200).json({
+    //   today: {
+    //     product:
+    //       result.products,
+    //   },
+    // });
   } catch (error) {res.status(500).json({ message: error.message });}
 }
 
@@ -129,19 +124,20 @@ const deleteConsumeProduct = async (req, res, next) => {
   try {
     const { todayId, productId } = req.params;
 
-    const result = await Today.findById(todayId);
-    if(!result) {return res.status(404).json({ message: 'Date Not Found' });}
+    const resultToday = await Today.findById(todayId);
+    if(!resultToday) {return res.status(404).json({ message: 'Date Not Found' });}
 
-    const grams = result.product.get(productId);
-    if(!result.product.has(productId)) {return res.status(404).json({ message: 'Product ID not Found' });}
-    result.product.delete(productId);
+    // const grams = result.products.get(productId);
+    // if(!result.products.has(productId)) {return res.status(404).json({ message: 'Product ID not Found' });}
+    // const resultProduct = await
+    result.products.delete(productId);
     await result.save();
-
-    res.status(200).json({
-      today: {
-        [productId]: grams,
-      },
-    });
+    res.status(200).json(result);
+    // res.status(200).json({
+    //   today: {
+    //     [productId]: grams,
+    //   },
+    // });
     
   } catch (error) {res.status(500).json({ message: error.message });}
 }
