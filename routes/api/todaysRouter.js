6f770a8c891c505821e39dded6f09126842c2514
Arctaea <joycacao@gmail.com>
2024-11-03 19:08:50 +0800

    route params adjust from today Id to Date

--- routes/api/todaysRouter.js
+++ routes/api/todaysRouter.js
@@ -5,10 +5,14 @@ import { authenticateToken } from '../../middlewares/auth.js';
 const router = express.Router();
 
 router.get('/', authenticateToken, getAllTodays);
-router.get('/:todayId', authenticateToken, getTodayById);
+// router.get('/:todayId', authenticateToken, getTodayById);
+router.get('/:todayDate', authenticateToken, getTodayById);
 router.delete('/:todayId', authenticateToken, deleteTodayById);
 router.post('/', authenticateToken, dailyIntake);
-router.patch('/:todayId', authenticateToken, consumeProduct);
+// router.patch('/:todayId', authenticateToken, consumeProduct);
+// router.patch('/:todayDate', authenticateToken, consumeProduct);
+// router.patch('/', authenticateToken, consumeProduct);
+router.patch('/:todayDate', authenticateToken, consumeProduct);
 router.delete('/:todayId/:productId', authenticateToken, deleteConsumeProduct);
 
 export default router;
